7e510daad4211240e1d2071e2457298ad05654c4
Kriti523 <kritijain9414@gmail.com>
2024-09-05 11:12:47 +0530

    added resume

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Upcoming Software Engineer", "Web Developer", "Web Designer"];
+  const toRotate = [
+    "Upcoming Software Engineer",
+    "Web Developer",
+    "Web Designer",
+  ];
   const period = 1000;
 
   useEffect(() => {
@@ -19,35 +23,42 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
   const openResume = () => {
-    window.open('resume-url.pdf', '_blank');
+    window.open(
+      "https://drive.google.com/file/d/1vtiNgQFc3B5d9EwRsCQWSeWh_9CIe-7_/view",
+      "_blank"
+    );
   };
 
   return (
@@ -56,30 +67,57 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Kriti |`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Hi, I’m Kriti, a B.Tech CSE student with a passion for tech and a zest for life. 
-                    I’m a foodie always on the lookout for new flavors and a math enthusiast who loves solving problems.
-                     My travels spark my curiosity, and my creative side shines through my designs on Canva. Whether I'm singing my 
-                     heart out or exploring new ideas, I bring joy and enthusiasm to everything I do. Join me as I blend my technical skills
-                      with creativity, adventure, and a love for life!
-</p>
-                  <button  variant="primary" onClick={openResume}> View My Resume </button>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Kriti |`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "Web Developer", "Web Designer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    Hi, I’m Kriti, a B.Tech CSE student with a passion for tech
+                    and a zest for life. I’m a foodie always on the lookout for
+                    new flavors and a math enthusiast who loves solving
+                    problems. My travels spark my curiosity, and my creative
+                    side shines through my designs on Canva. Whether I'm singing
+                    my heart out or exploring new ideas, I bring joy and
+                    enthusiasm to everything I do. Join me as I blend my
+                    technical skills with creativity, adventure, and a love for
+                    life!
+                  </p>
+                  <button variant="primary" onClick={openResume}>
+                    {" "}
+                    View My Resume{" "}
+                  </button>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
